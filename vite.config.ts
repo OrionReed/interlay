2a1bea60322d8311942c59b2a31d64d28bfaee5f
Orion Reed <orionreedt@gmail.com>
2024-04-01 12:03:00 -0700

    simplified config a ton

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import topLevelAwait from "vite-plugin-top-level-await";
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
     react(),
-    topLevelAwait(),
     viteStaticCopy({
       targets: [
         {
@@ -22,14 +20,12 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       output: {
-        // Configure output file names here
         entryFileNames: "[name].js", // For entry chunks
         chunkFileNames: "[name].js", // For non-entry chunks
         assetFileNames: "[name].[ext]" // For static assets like images and styles
       }
     }
   },
-  publicDir: 'src/public',
   resolve: {
     alias: {
       '@': '/src',
