71ef31851fc0dde0c3a0ff9091a19a3a9bdda049
Orion Reed <orionreedt@gmail.com>
2024-04-03 06:50:17 -0700

    remove CSP headers (dangerous but good)

--- src/background.js
+++ src/background.js
@@ -9,3 +9,21 @@ browser.browserAction.onClicked.addListener((tab) => {
 		func: () => window.postMessage({ action: "enableInterlay" }, "*"),
 	});
 });
+
+browser.webRequest.onHeadersReceived.addListener(
+	(details) => {
+		for (let i = 0; i < details.responseHeaders.length; i++) {
+			if (
+				["content-security-policy", "x-content-security-policy"].includes(
+					details.responseHeaders[i].name.toLowerCase(),
+				)
+			) {
+				details.responseHeaders.splice(i, 1); // Remove CSP header
+				break; // Assuming only one CSP header, break after finding it
+			}
+		}
+		return { responseHeaders: details.responseHeaders };
+	},
+	{ urls: ["<all_urls>"] }, // Use specific URL patterns to limit scope
+	["blocking", "responseHeaders"],
+);
