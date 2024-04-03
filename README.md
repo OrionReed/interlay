5b70e41cc2a58720844fe0cf15c47229e00c44b2
Orion Reed <orionreedt@gmail.com>
2024-04-02 19:09:24 -0700

    fixed icon

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Decompose the web.
+# INTERLAY
+Decompose the web?
+
+**In Firefox:**
+1. `yarn install`
+2. `yarn watch`
+3. go to `about:addons` and enable `Run on sites with restrictions` 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
-import webExtension from "vite-plugin-web-extension"
+import webExtension, { readJsonFile } from "vite-plugin-web-extension"
 
 export default defineConfig({
   plugins: [
     react(),
     webExtension({
-      browser: 'firefox'
+      browser: 'firefox',
+      manifest: () => {
+        // Use `readJsonFile` instead of import/require to avoid caching during rebuild.
+        const pkg = readJsonFile("package.json");
+        const template = readJsonFile("manifest.json");
+        return {
+          ...template,
+          version: pkg.version,
+          name: pkg.name,
+          description: pkg.description,
+          author: pkg.author,
+        };
+      },
     }),
     viteStaticCopy({
       targets: [
